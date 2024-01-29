050a3b99b31480c522f9a790fb4c9f3a9a254584
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-01-29 21:09:21 +0500

    my custom text button widget

--- lib/res/widgets/my_text_button_widget.dart
+++ lib/res/widgets/my_text_button_widget.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:linkedinclone/res/widgets/my_text_widget.dart';
 
 class MyTextButton extends StatelessWidget {
-  const MyTextButton({super.key});
+  final String title;
+  final double? fontSize;
+  final Color? textColor;
+  final FontWeight? fontWeight;
+  final Color? backgroundColor;
+  final double? width;
+  final double? height;
+  final BorderRadius? borderRadius;
+  final Alignment? alignment;
+  final void Function()? onTap;
+
+    MyTextButton(
+      {super.key,
+      required this.title,
+      this.backgroundColor,
+      this.fontSize,
+      this.fontWeight,
+      this.textColor,
+      this.width,
+      this.height,
+      this.borderRadius,
+      this.alignment,
+      this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        alignment: alignment,
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          borderRadius: borderRadius ?? BorderRadius.circular(12)
+        ),
+        child: MyText(
+          title: title,
+          fontWeight: fontWeight,
+          fontSize: fontSize,
+          color: textColor,
+        ),
+      ),
+    );
   }
 }
