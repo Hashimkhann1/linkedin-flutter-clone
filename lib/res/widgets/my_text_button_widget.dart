bde4ef404108d8978edca1822e9bba4f8d7a0eb9
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-01-29 21:06:53 +0500

    fullname linkedin logo

---
+++ lib/res/widgets/my_text_button_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyTextButton extends StatelessWidget {
+  const MyTextButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
