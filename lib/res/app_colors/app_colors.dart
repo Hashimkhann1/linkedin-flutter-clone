04835b9b09dff946b38e6208045500f9fbb46b61
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-01-29 21:08:37 +0500

    all app color is defined in this file

---
+++ lib/res/app_colors/app_colors.dart
@@ -0,0 +1,13 @@
+
+
+
+
+import 'package:flutter/material.dart';
+
+class AppCollors {
+
+  static Color primaryColor = const Color(0xff0c63bf);
+  static Color whiteColor = const Color(0xffffffff);
+  static Color grayColor = Colors.grey;
+
+}
