349faed80f47218974edc1224abc6eeda7ecac3d
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-01-30 21:31:43 +0500

    new image path is added

--- lib/res/constant.dart
+++ lib/res/constant.dart
@@ -3,4 +3,6 @@
 class Constant {
   static String logo = 'assets/images/linkedin.png';
   static String fullnamelogo = 'assets/images/fullnamelogo.png';
+  static String messages = 'assets/images/write_11477188';
+
 }
