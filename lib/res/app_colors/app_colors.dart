7a28f9794ce88a26c5d2c1dfb8630b97077c075e
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-01-30 21:26:51 +0500

    new coloers is added

--- lib/res/app_colors/app_colors.dart
+++ lib/res/app_colors/app_colors.dart
@@ -7,7 +7,10 @@ import 'package:flutter/material.dart';
 class AppCollors {
 
   static Color primaryColor = const Color(0xff0c63bf);
+  static Color backgroundColor = const Color(0xfff4f2ee);
   static Color whiteColor = const Color(0xffffffff);
   static Color grayColor = Colors.grey;
+  static Color lightGrayColor = Color(0xffedf3f9);
+  static Color blackColor = Colors.black;
 
 }
