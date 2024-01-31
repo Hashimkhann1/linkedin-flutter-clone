efb7e28780e0601c881af099aed52c7802a3a401
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-01-31 23:27:07 +0500

    this is app bar widget bbut i'm not using this in any file

--- lib/res/widgets/my_app_bar_widgets.dart
+++ lib/res/widgets/my_app_bar_widgets.dart
@@ -7,37 +7,54 @@ class MyAppBarView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      backgroundColor: AppCollors.whiteColor,
-      leading: InkWell(
-          onTap: (){
+    return Container(
+      decoration: BoxDecoration(
+        color: AppCollors.whiteColor,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      child: AppBar(
+        backgroundColor: AppCollors.whiteColor,
+        leading: InkWell(
+          onTap: () {
             Scaffold.of(context).openEndDrawer();
           },
           child: Padding(
             padding: const EdgeInsets.all(10.0),
-            child: Image.network('https://cdn-icons-png.flaticon.com/128/4140/4140061.png',),
-          )
-      ),
-      title: Container(
-        height: 30,
-        decoration: BoxDecoration(
+            child: Image.network(
+              'https://cdn-icons-png.flaticon.com/128/4140/4140061.png',
+            ),
+          ),
+        ),
+        title: Container(
+          height: 30,
+          decoration: BoxDecoration(
             color: AppCollors.lightGrayColor,
-            borderRadius: BorderRadius.circular(6)
+            borderRadius: BorderRadius.circular(6),
+          ),
+          child: Row(
+            children: [
+              const SizedBox(width: 6),
+              Icon(
+                Icons.search,
+                color: AppCollors.grayColor,
+              ),
+              const SizedBox(width: 4),
+              MyText(title: "Search", fontSize: 14, color: AppCollors.grayColor),
+            ],
+          ),
         ),
-        child: Row(
-          children: [
-            const SizedBox(width: 6,),
-            Icon(Icons.search,color: AppCollors.grayColor,),
-            const SizedBox(width: 4),
-            MyText(title: "Search",fontSize: 14,color: AppCollors.grayColor,)
-          ],
-        ),
-
+        actions: [
+          Icon(Icons.message, color: AppCollors.grayColor),
+          const SizedBox(width: 8),
+        ],
       ),
-      actions: [
-        Icon(Icons.message,color: AppCollors.grayColor,),
-        const SizedBox(width: 8,),
-      ],
     );
   }
 }
