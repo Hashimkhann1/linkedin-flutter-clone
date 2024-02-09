a2b8594a6ddde738577e71c6f551dbcc70ae9b57
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-02-09 21:15:08 +0500

    checking user is already login or not

--- lib/view_model/splash_view_model/splash_view_model.dart
+++ lib/view_model/splash_view_model/splash_view_model.dart
@@ -1,15 +1,19 @@
 
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:linkedinclone/view/auth_view/singin_view/signin_view.dart';
 import 'package:linkedinclone/view/bottom_navigatore_view.dart';
 
 class SplashViewModel {
 
+  final _auth = FirebaseAuth.instance.currentUser;
+
   void splashTime(BuildContext context) async {
     await Future.delayed(Duration(seconds: 3), () {});
 
-    Navigator.push(context, MaterialPageRoute(builder: (context) => SignInView()));
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) => _auth != null ? BottomNavigatoreView() : SignInView()));
 
   }
 
