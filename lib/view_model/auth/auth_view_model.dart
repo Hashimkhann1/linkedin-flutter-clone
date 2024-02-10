2903f1ce224f4e0524826546738f37ce7a70b142
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-02-10 15:36:37 +0500

    authentication is completed

--- lib/view_model/auth/auth_view_model.dart
+++ lib/view_model/auth/auth_view_model.dart
@@ -1,47 +1,77 @@
 
 
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:get/get.dart';
+import 'package:linkedinclone/res/app_colors/app_colors.dart';
+import 'package:linkedinclone/res/constant.dart';
+import 'package:linkedinclone/view/auth_view/singin_view/signin_view.dart';
+import 'package:linkedinclone/view/bottom_navigatore_view.dart';
+import 'package:linkedinclone/view_model/getx/loading_getx/loading_getx.dart';
 
 class AuthViewModel {
 
   final _auth = FirebaseAuth.instance;
+  // initilaing LoadinGetX
+  final LoadingGetx loadingGetx = Get.put(LoadingGetx());
+
 
   // signIn method
   void signIn(BuildContext context , String gmail , password) async {
+    loadingGetx.setLoading();
     try{
       await _auth.signInWithEmailAndPassword(email: gmail, password: password).then((value) {
-
-      }).onError((error, stackTrace){
-
+        loadingGetx.setLoading();
+        toastMessage("Login successfull");
+        Navigator.push(context, MaterialPageRoute(builder: (context) => BottomNavigatoreView()));
       });
 
-    }catch(error){
+    }on FirebaseAuthException catch(error){
+      loadingGetx.setLoading();
+      toastMessage(error.code.toString());
       print("error while siging in from AuthViewModel $error");
     }
   }
 
   // signUp method
   void signUp(BuildContext context , String gmail , password) async {
+    loadingGetx.setLoading();
     try{
       await _auth.createUserWithEmailAndPassword(email: gmail, password: password).then((value){
-
-      }).onError((error, stackTrace) {
-
+        loadingGetx.setLoading();
+        toastMessage("Sign Up successfull");
+        Navigator.push(context, MaterialPageRoute(builder: (context) => BottomNavigatoreView()));
       });
-    }catch(error){
+    }on FirebaseAuthException catch(error){
+      loadingGetx.setLoading();
+      toastMessage(error.code.toString());
       print("error while siging Up from AuthViewModel $error");
     }
   }
 
   // signOut method
-  void signOut() async {
+  void signOut(BuildContext context) async {
     try{
-      await _auth.signOut();
+      await _auth.signOut().then((value) {
+        toastMessage("User SignOut Successfully");
+        Navigator.push(context, MaterialPageRoute(builder: (context) => SignInView()));
+      });
     }catch(error){
       print("error while siging out from AuthViewModel $error");
     }
+  }
+
+
+  toastMessage(String message) {
+    return Fluttertoast.showToast(
+      msg: message.toString(),
+      backgroundColor: AppCollors.primaryColor,
+      textColor: AppCollors.whiteColor,
+      timeInSecForIosWeb: 1,
+      toastLength: Toast.LENGTH_LONG,
+    );
   }
 
 }
