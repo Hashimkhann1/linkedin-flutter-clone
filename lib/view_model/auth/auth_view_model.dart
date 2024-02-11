0a8be56977822f44e7b7d2bb475dba17e2479642
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-02-11 14:50:07 +0500

    adding user initial data method is added to add user data while signing up

--- lib/view_model/auth/auth_view_model.dart
+++ lib/view_model/auth/auth_view_model.dart
@@ -1,5 +1,6 @@
 
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -14,8 +15,22 @@ import 'package:linkedinclone/view_model/getx/loading_getx/loading_getx.dart';
 class AuthViewModel {
 
   final _auth = FirebaseAuth.instance;
+  final _firestore = FirebaseFirestore.instance.collection('users');
   // initilaing LoadinGetX
   final LoadingGetx loadingGetx = Get.put(LoadingGetx());
+
+  // save initial user data
+  initialUserData(String gmail) async {
+    try{
+    await _firestore.doc(_auth.currentUser!.uid).set({
+      "userGmail": gmail.toString(),
+      "userUid": _auth.currentUser!.uid,
+      "DateTime": DateTime.now(),
+    });
+  }catch(e){
+      print("Error while adding initial user data from  AuthViewModel $e");
+    }
+  }
 
 
   // signIn method
@@ -42,6 +57,7 @@ class AuthViewModel {
       await _auth.createUserWithEmailAndPassword(email: gmail, password: password).then((value){
         loadingGetx.setLoading();
         toastMessage("Sign Up successfull");
+        initialUserData(gmail);
         Navigator.push(context, MaterialPageRoute(builder: (context) => BottomNavigatoreView()));
       });
     }on FirebaseAuthException catch(error){
