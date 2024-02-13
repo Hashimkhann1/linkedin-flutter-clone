e4627d38d6cb68c780aff09cab03be61baf3c953
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-02-13 17:44:17 +0500

    getting logedin user data

---
+++ lib/view_model/get_logedin_user_data_view_model/get_logedin_user_data_view_model.dart
@@ -0,0 +1,31 @@
+
+
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+import 'package:linkedinclone/model/logedin_user_data_model/logedin_user_data_model.dart';
+import 'package:linkedinclone/view_model/logedin_user_data_list_view_model/logedin_user_data_list_view_model.dart';
+
+class GetLogedinUserDataViewModel {
+
+  final _auth = FirebaseAuth.instance;
+  final _firestore = FirebaseFirestore.instance.collection('users');
+
+  final LogedinUserDataListViewModel logedinUserDataListViewModel = Get.put(LogedinUserDataListViewModel());
+
+  void getLogedinUserData() async {
+    try{
+      DocumentSnapshot<Map<String, dynamic>> data = await _firestore.doc(_auth.currentUser!.uid.toString()).get();
+
+      if(data.data() != null){
+        print(data.data());
+        dynamic userData = await LogedinUserDataModel.fromJson(data.data()!);
+        logedinUserDataListViewModel.logedinUserDataList.add(userData);
+      }
+    }catch(error){
+      print('error while getting logedin user data from GetLogedinUserDataViewModel $error');
+    }
+  }
+
+}
