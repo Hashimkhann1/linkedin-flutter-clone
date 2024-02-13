4a3aa64a718b2b72baca3bd4ea5536ed35a13e05
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-02-13 17:46:23 +0500

    get logedin user data function is added

--- lib/view_model/splash_view_model/splash_view_model.dart
+++ lib/view_model/splash_view_model/splash_view_model.dart
@@ -4,14 +4,18 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:linkedinclone/view/auth_view/singin_view/signin_view.dart';
 import 'package:linkedinclone/view/bottom_navigatore_view.dart';
+import 'package:linkedinclone/view_model/get_logedin_user_data_view_model/get_logedin_user_data_view_model.dart';
 
 class SplashViewModel {
 
-  final _auth = FirebaseAuth.instance.currentUser;
+  final _auth = FirebaseAuth.instance;
+  final GetLogedinUserDataViewModel logedinUserDataViewModel = GetLogedinUserDataViewModel();
 
   void splashTime(BuildContext context) async {
     await Future.delayed(Duration(seconds: 3), () {});
-
+    if(_auth != null){
+      logedinUserDataViewModel.getLogedinUserData();
+    }
 
     Navigator.push(context, MaterialPageRoute(builder: (context) => _auth != null ? BottomNavigatoreView() : SignInView()));
 
